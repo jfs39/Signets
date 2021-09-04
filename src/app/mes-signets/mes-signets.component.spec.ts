2c2c2ee476790c0de57f4e90f15f95a4863540c6
jfs39 <jfs.serg@hotmail.com>
2021-09-03 21:08:41 -0400

    First commit

---
+++ src/app/mes-signets/mes-signets.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MesSignetsComponent } from './mes-signets.component';
+
+describe('MesSignetsComponent', () => {
+  let component: MesSignetsComponent;
+  let fixture: ComponentFixture<MesSignetsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MesSignetsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MesSignetsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/mes-signets/mes-signets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mes-signets',
+  templateUrl: './mes-signets.component.html',
+  styleUrls: ['./mes-signets.component.css']
+})
+export class MesSignetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
